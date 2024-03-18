347d8e45bb328fa90d9aaa77597f34efe2f27e18
penglin2 <penglin2@iflytek.com>
2024-03-18 10:44:33 +0800

    update

--- client.py
+++ client.py
@@ -17,23 +17,25 @@ def on_error(ws, error):
 
 
 class Client:
-    def __init__(self, appid, apikey, api_secret, endpoint, uri):
+    def __init__(self, appid, apikey, api_secret, host, uri, tls=False):
         self.appid = appid
         self.apikey = apikey
         self.api_secret = api_secret
-        self.endpoint = endpoint
+        self.host = host
         self.uri = uri
+        self.tls = tls
 
     def once(self, body, headers):
-        return requests.post(url=self.build_signed_url(self.endpoint, self.uri), data=body, headers=headers)
+        return requests.post(url=self.build_signed_url(), data=body, headers=headers)
     
     def stream(self, on_open, on_message, on_error=on_error):
-            url = self.build_signed_url(self.endpoint, self.uri, 'GET')
+            url = self.build_signed_url(method="GET")
             self.ws = websocket.WebSocketApp(url, on_open=on_open, on_message=on_message, on_error=on_error)
             self.ws.run_forever()
     
-    def build_signed_url(self, endpoint, uri, method='POST'):
-        url_result = parse.urlparse(endpoint+uri)
+    def build_signed_url(self, method='POST'):
+        url_raw = scheme(method, self.tls)+self.host+self.uri
+        url_result = parse.urlparse(url_raw)
         date = format_date_time(mktime(datetime.now().timetuple()))
         signature_origin = "host: {}\ndate: {}\n{} {} HTTP/1.1".format(url_result.hostname, date, method, url_result.path)
         signature_sha = hmac.new(self.api_secret.encode('utf-8'), signature_origin.encode('utf-8'),digestmod= sha256).digest()
@@ -46,4 +48,18 @@ class Client:
             "date": date,
             "authorization": authorization
         }
-        return endpoint + uri + "?" + urlencode(values)
+        return url_raw + "?" + urlencode(values)
+    
+def scheme(method, tls) -> str:
+         if method == "POST":
+              if tls:
+                return "https://"
+              else:
+                return "http://"
+              
+         elif method == "GET":
+             if tls:
+                 return "wss://"
+             else:
+                 return "ws://"
+              
